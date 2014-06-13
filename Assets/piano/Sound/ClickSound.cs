812131941e1c4275e3c363d1877a2430c0d56433
王瀚宇 <huw12313212@gmail.com>
2014-06-13 22:44:18 +0800

    +LeapPiano

---
+++ Assets/piano/Sound/ClickSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickSound : MonoBehaviour {
+
+	public AudioSource audioSource;
+
+	bool clicked = false;
+
+	int  ThreashHoldZ = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+		audioSource = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!clicked&&transform.localRotation.eulerAngles.z > 1) 
+		{
+			clicked = true;
+
+			audioSource.Play();
+		}
+		else if(clicked&&transform.localRotation.eulerAngles.z <= 1)
+		{
+			clicked = false;
+			
+			audioSource.Stop();
+		}
+
+
+	}
+}
